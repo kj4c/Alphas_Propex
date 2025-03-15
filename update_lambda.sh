ccc06638609dc8d9d4ba193392c4803c1f954951
kj4c <lowkhyejac@gmail.com>
2025-03-16 00:29:32 +1100

    script now skips functions that are not modified

--- update_lambda.sh
+++ update_lambda.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-BACKEND_DIR="backend"
+BACKEND_DIR="./backend"
 CHANGED=0
 
-echo "Starting Lambda ZIP process..."
+echo "🚀 Starting Lambda ZIP process..."
+echo ""
 
 for FUNCTION_DIR in "$BACKEND_DIR"/*; do
     if [ -d "$FUNCTION_DIR" ]; then
         FUNCTION_NAME=$(basename "$FUNCTION_DIR")
+        if [ "$FUNCTION_NAME" = "tests" ]; then
+            continue
+        fi
+
         ZIP_FILE="$FUNCTION_DIR/lambda.zip"
         TEMP_CHECKSUM_FILE="$FUNCTION_DIR/.checksum"
 
-        NEW_CHECKSUM=$(find "$FUNCTION_DIR" -type f ! -name "lambda.zip" -exec md5sum {} + | sort -k 2 | md5sum | awk '{print $1}')
+        NEW_CHECKSUM=$(md5sum "$FUNCTION_DIR"/handler.py "$FUNCTION_DIR"/helpers.py 2>/dev/null | md5sum | awk '{print $1}')
+
 
         if [ ! -f "$TEMP_CHECKSUM_FILE" ]; then
             touch "$TEMP_CHECKSUM_FILE"
@@ -24,13 +30,13 @@ for FUNCTION_DIR in "$BACKEND_DIR"/*; do
             echo "📦 Zipping function: $FUNCTION_NAME..."
             cd "$FUNCTION_DIR" || exit
             zip -r -X lambda.zip . helpers.py > /dev/null
-            # stores the new CHECKSUM FILE
-            echo "$NEW_CHECKSUM" > "$TEMP_CHECKSUM_FILE"
             cd - > /dev/null
+            # stores the new CHECKSUM FILE
+            echo "$NEW_CHECKSUM" | tee "$TEMP_CHECKSUM_FILE" > /dev/null
             echo "✅ Created: $ZIP_FILE"
             CHANGED=1
         else 
-            echo "✅ No change detected for $FUNCTION_NAME, skipping da zip!"
+            echo "🐼 No change detected for $FUNCTION_NAME, skipping da zip!"
         fi
     fi
 done
@@ -40,9 +46,9 @@ if [ "$CHANGED" -eq 1 ]; then
     python3 update_variables.py
 
     echo "🌍 Applying Terraform changes..."
-    # terraform apply -auto-approve
+    terraform apply -auto-approve
 
     echo "🎉 Deployment complete!"
 else
-    echo "✅ No lambda functions updated, skipping terraform apply"
+    echo "🐻 No lambda functions updated, skipping terraform apply"
 fi
