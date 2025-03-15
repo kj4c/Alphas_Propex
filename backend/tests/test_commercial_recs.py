cf165698465ac5de4e638e4cb864faab7a01433c
kj4c <lowkhyejac@gmail.com>
2025-03-15 21:23:52 +1100

    added tests and CI/CD

---
+++ backend/tests/test_commercial_recs.py
@@ -0,0 +1,7 @@
+import unittest
+from commercial_recs.helpers import find_commerical_recs
+
+class TestCommercialRecs(unittest.TestCase):
+    def test_input1(self):
+        # test given 3 properties
+        self.assertEqual(find_commerical_recs("poop"), "yo")
