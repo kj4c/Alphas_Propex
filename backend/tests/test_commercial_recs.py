9fbb135db1b532509291cb9913a2df9cc1f45d5c
kj4c <lowkhyejac@gmail.com>
2025-03-16 00:34:42 +1100

    modified commercial_recs

--- backend/tests/test_commercial_recs.py
+++ backend/tests/test_commercial_recs.py
@@ -4,4 +4,4 @@ from commercial_recs.helpers import find_commerical_recs
 class TestCommercialRecs(unittest.TestCase):
     def test_input1(self):
         # test given 3 properties
-        self.assertEqual(find_commerical_recs("poop"), "yooo")
+        self.assertEqual(find_commerical_recs("poop"), "yoooo")
