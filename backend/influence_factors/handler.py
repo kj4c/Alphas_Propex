94d36eb5465c6fda64ee2f3592c4285d702772c1
Joel Lum <joellumzh@gmail.com>
2025-03-18 13:49:13 +1100

    Moved property class from influence factor handler to general classes file

--- backend/influence_factors/handler.py
+++ backend/influence_factors/handler.py
@@ -1,18 +1,7 @@
 import json
 from helpers import find_influence_factors
-from general_helpers import to_dataframe, fetch_data
-from enum import Enum
-
-class PropertyType(Enum):
-    HOUSE = "House"
-    APARTMENT = "Apartment"
-    DUPLEX = "Duplex"
-    TOWNHOUSE = "Townhouse"
-    VACANT_LAND = "Vacant Land"
-    SEMIDETACHED = "Semi-Detached"
-    NEWHOUSEANDLAND = "New House & Land"
-    UNIT = "Unit"
-    FLAT = "Flat"
+from general_helpers import to_dataframe
+from classes import PropertyType
 
 def lambda_handler(event, context):
     """
