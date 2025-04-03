663d7df1ac00ac31b5b7311ac01ab3fa9f223441
Eric Choe <z5345955@ad.unsw.edu.au>
2025-04-03 19:14:35 +1100

    changed front end request body to include function_name

--- frontend/src/pages/PropertyPrices.jsx
+++ frontend/src/pages/PropertyPrices.jsx
@@ -1,197 +1,254 @@
-import { useEffect, useState } from "react"
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 const PropertyPrices = () => {
-    const [loading, setLoading] = useState(false)
-    const [loaded, setLoaded] = useState(false)
-    const [price, setPrice] = useState("")
-    const [id, setId] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [price, setPrice] = useState("");
+  const [id, setId] = useState(null);
 
-    const [minPrice, setMinPrice] = useState(null)
-    const [maxPrice, setMaxPrice] = useState(null)
-    const [dateSoldAfter, setDateSoldAfter] = useState(null)
-    const [dateSoldBefore, setDateSoldBefore] = useState(null)
-    const [suburb, setSuburb] = useState(null)
-    const [numBath, setNumBath] = useState(null)
-    const [numBed, setNumBed] = useState(null)
-    const [numParking, setNumParking] = useState(null)
-    const [minSize, setMinSize] = useState(null)
-    const [maxSize, setMaxSize] = useState(null)
-    const [type, setType] = useState(null)
+  const [minPrice, setMinPrice] = useState(null);
+  const [maxPrice, setMaxPrice] = useState(null);
+  const [dateSoldAfter, setDateSoldAfter] = useState(null);
+  const [dateSoldBefore, setDateSoldBefore] = useState(null);
+  const [suburb, setSuburb] = useState(null);
+  const [numBath, setNumBath] = useState(null);
+  const [numBed, setNumBed] = useState(null);
+  const [numParking, setNumParking] = useState(null);
+  const [minSize, setMinSize] = useState(null);
+  const [maxSize, setMaxSize] = useState(null);
+  const [type, setType] = useState(null);
 
-    const fetchPrice = async () => {
-        if (id == null){
-            alert("missing id")
-            return
-        }
-        setLoaded(false)
-        const filters = {
-          price_range: {
-            min_price: parseFloat(minPrice) || null,
-            max_price: parseFloat(maxPrice) || null,
-          },
-          date_range: {
-            date_sold_after: dateSoldAfter || null,
-            date_sold_before: dateSoldBefore || null,
-          },
-          suburb: suburb || null,
-          property_features: {
-            num_bath: parseInt(numBath) || null,
-            num_bed: parseInt(numBed) || null,
-            num_parking: parseInt(numParking) || null,
-          },
-          property_size: {
-            min_size: parseInt(minSize) || null,
-            max_size: parseInt(maxSize) || null,
-          },
-          property_type: type || null,
-        };
-      
-        const requestBody = {
-          id: id,
-          filters: filters,
-        };
-
-        setLoading(true);
-      
-        try {
-          const response = await axios.post(
-            "https://q50eubtwpj.execute-api.us-east-1.amazonaws.com/property_prices",
-            requestBody,
-            {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-
-          setLoaded(true);
-          setLoading(false);
-          setPrice(response.data.avg_price);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-          setLoading(false);
-        }
+  const fetchPrice = async () => {
+    if (id == null) {
+      alert("missing id");
+      return;
+    }
+    setLoaded(false);
+    const filters = {
+      price_range: {
+        min_price: parseFloat(minPrice) || null,
+        max_price: parseFloat(maxPrice) || null,
+      },
+      date_range: {
+        date_sold_after: dateSoldAfter || null,
+        date_sold_before: dateSoldBefore || null,
+      },
+      suburb: suburb || null,
+      property_features: {
+        num_bath: parseInt(numBath) || null,
+        num_bed: parseInt(numBed) || null,
+        num_parking: parseInt(numParking) || null,
+      },
+      property_size: {
+        min_size: parseInt(minSize) || null,
+        max_size: parseInt(maxSize) || null,
+      },
+      property_type: type || null,
     };
-    
 
-    return (
-        <div className="page">
-            <h1>Average Property Prices</h1>
-            <p>Id:</p>
-            <input type="text" name="id" placeholder="Id" onChange={e => {
-                if (e.target.value !== "") {
-                    setId(e.target.value)
-                } else {
-                    setId(null)
-                }
-            }}/>
-            <div className="filter-field">
-                <p>Price range:</p>
-                <input type="text" name="min-price" placeholder="Min price" onChange={e => {
-                    if (e.target.value !== "") {
-                        setMinPrice(e.target.value)
-                    } else {
-                        setMinPrice(null)
-                    }
-                }}/>
-                <input type="text" name="max-price" placeholder="Max price" onChange={e => {
-                    if (e.target.value !== "") {
-                        setMaxPrice(e.target.value)
-                    } else {
-                        setMaxPrice(null)
-                    }
-                }}/>
-            </div>
-            <div className="filter-field">
-                <p>Date range:</p>
-                <input type="text" name="sold-after" placeholder="Date sold after" onChange={e => {
-                    if (e.target.value !== "") {
-                        setDateSoldAfter(e.target.value)
-                    } else {
-                        setDateSoldAfter(null)
-                    }
-                }}/>
-                <input type="text" name="sold-before" placeholder="Date sold before" onChange={e => {
-                    if (e.target.value !== "") {
-                        setDateSoldBefore(e.target.value)
-                    } else {
-                        setDateSoldBefore(null)
-                    }
-                }}/>
-            </div>
-            <div className="filter-field">
-                <p>Suburb:</p>
-                <input type="text" name="sold-after" placeholder="Suburb" onChange={e => {
-                    if (e.target.value !== "") {
-                        setSuburb(e.target.value)
-                    } else {
-                        setSuburb(null)
-                    }
-                }}/>
-            </div>
-            <div className="filter-field">
-                <p>Property feature:</p>
-                <input type="text" name="num-bath" placeholder="Bathrooms" onChange={e => {
-                    if (e.target.value !== "") {
-                        setNumBath(e.target.value)
-                    } else {
-                        setNumBath(null)
-                    }
-                }}/>
-                <input type="text" name="num-bed" placeholder="Bedrooms" onChange={e => {
-                    if (e.target.value !== "") {
-                        setNumBed(e.target.value)
-                    } else {
-                        setNumBed(null)
-                    }
-                }}/>
-                <input type="text" name="num-parking" placeholder="Parking" onChange={e => {
-                    if (e.target.value !== "") {
-                        setNumParking(e.target.value)
-                    } else {
-                        setNumParking(null)
-                    }
-                }}/>
-            </div>
-            <div className="filter-field">
-                <p>Property size:</p>
-                <input type="text" name="min-size" placeholder="Min size" onChange={e => {
-                    if (e.target.value !== "") {
-                        setMinSize(e.target.value)
-                    } else {
-                        setMinSize(null)
-                    }
-                }}/>
-                <input type="text" name="max-size" placeholder="Max size" onChange={e => {
-                    if (e.target.value !== "") {
-                        setMaxSize(e.target.value)
-                    } else {
-                        setMaxSize(null)
-                    }
-                }}/>
-            </div>
-            <div className="filter-field">
-                <p>Property type:</p>
-                <input type="text" name="min-size" placeholder="Type" onChange={e => {
-                    if (e.target.value !== "") {
-                        setType(e.target.value)
-                    } else {
-                        setType(null)
-                    }
-                }}/>
-            </div>
-            <button onClick={fetchPrice}>Search</button>
-            {
-                loading && <div className="loading">loading...</div>
+    const requestBody = {
+      id: id,
+      filters: filters,
+      function_name: "property_prices",
+    };
+
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "https://q50eubtwpj.execute-api.us-east-1.amazonaws.com/property_prices",
+        requestBody,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      setLoaded(true);
+      setLoading(false);
+      setPrice(response.data.avg_price);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="page">
+      <h1>Average Property Prices</h1>
+      <p>Id:</p>
+      <input
+        type="text"
+        name="id"
+        placeholder="Id"
+        onChange={(e) => {
+          if (e.target.value !== "") {
+            setId(e.target.value);
+          } else {
+            setId(null);
+          }
+        }}
+      />
+      <div className="filter-field">
+        <p>Price range:</p>
+        <input
+          type="text"
+          name="min-price"
+          placeholder="Min price"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setMinPrice(e.target.value);
+            } else {
+              setMinPrice(null);
             }
-            {
-                loaded && 
-                <div className="avg-price"> 
-                    <h2> Average price: {price}</h2>
-                </div>
+          }}
+        />
+        <input
+          type="text"
+          name="max-price"
+          placeholder="Max price"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setMaxPrice(e.target.value);
+            } else {
+              setMaxPrice(null);
             }
+          }}
+        />
+      </div>
+      <div className="filter-field">
+        <p>Date range:</p>
+        <input
+          type="text"
+          name="sold-after"
+          placeholder="Date sold after"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setDateSoldAfter(e.target.value);
+            } else {
+              setDateSoldAfter(null);
+            }
+          }}
+        />
+        <input
+          type="text"
+          name="sold-before"
+          placeholder="Date sold before"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setDateSoldBefore(e.target.value);
+            } else {
+              setDateSoldBefore(null);
+            }
+          }}
+        />
+      </div>
+      <div className="filter-field">
+        <p>Suburb:</p>
+        <input
+          type="text"
+          name="sold-after"
+          placeholder="Suburb"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setSuburb(e.target.value);
+            } else {
+              setSuburb(null);
+            }
+          }}
+        />
+      </div>
+      <div className="filter-field">
+        <p>Property feature:</p>
+        <input
+          type="text"
+          name="num-bath"
+          placeholder="Bathrooms"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setNumBath(e.target.value);
+            } else {
+              setNumBath(null);
+            }
+          }}
+        />
+        <input
+          type="text"
+          name="num-bed"
+          placeholder="Bedrooms"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setNumBed(e.target.value);
+            } else {
+              setNumBed(null);
+            }
+          }}
+        />
+        <input
+          type="text"
+          name="num-parking"
+          placeholder="Parking"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setNumParking(e.target.value);
+            } else {
+              setNumParking(null);
+            }
+          }}
+        />
+      </div>
+      <div className="filter-field">
+        <p>Property size:</p>
+        <input
+          type="text"
+          name="min-size"
+          placeholder="Min size"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setMinSize(e.target.value);
+            } else {
+              setMinSize(null);
+            }
+          }}
+        />
+        <input
+          type="text"
+          name="max-size"
+          placeholder="Max size"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setMaxSize(e.target.value);
+            } else {
+              setMaxSize(null);
+            }
+          }}
+        />
+      </div>
+      <div className="filter-field">
+        <p>Property type:</p>
+        <input
+          type="text"
+          name="min-size"
+          placeholder="Type"
+          onChange={(e) => {
+            if (e.target.value !== "") {
+              setType(e.target.value);
+            } else {
+              setType(null);
+            }
+          }}
+        />
+      </div>
+      <button onClick={fetchPrice}>Search</button>
+      {loading && <div className="loading">loading...</div>}
+      {loaded && (
+        <div className="avg-price">
+          <h2> Average price: {price}</h2>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default PropertyPrices
+export default PropertyPrices;
--- frontend/src/pages/TopSchoolArea.jsx
+++ frontend/src/pages/TopSchoolArea.jsx
@@ -1,128 +1,135 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 const TopSchoolArea = () => {
-    const [loading, setLoading] = useState(false)
-    const [schools, setSchools] = useState(null)
-    const [option, setOption] = useState(0);
-    const [option1, setOption1] = useState(0);
-    const [radius, setRadius] = useState(10);
-    const [id, setId] = useState(null)
-    const fetchData = async () => {
-        if (id == null) {
-            alert("missing id")
-            return 
-        }
-        setLoading(true)
-        const requestBody = {
-            id: id,
-            radius: parseInt(radius) || 10,
-            district: districts[option1],
-            school_type: schoolTypes[option]
-        };
-
-        const response = await axios.post(
-            "https://q50eubtwpj.execute-api.us-east-1.amazonaws.com/top_school_area",
-            requestBody,
-            {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-        );
-        setLoading(false)
-        setSchools(JSON.parse(response.data.top_school_area))
+  const [loading, setLoading] = useState(false);
+  const [schools, setSchools] = useState(null);
+  const [option, setOption] = useState(0);
+  const [option1, setOption1] = useState(0);
+  const [radius, setRadius] = useState(10);
+  const [id, setId] = useState(null);
+  const fetchData = async () => {
+    if (id == null) {
+      alert("missing id");
+      return;
     }
+    setLoading(true);
+    const requestBody = {
+      id: id,
+      radius: parseInt(radius) || 10,
+      district: districts[option1],
+      school_type: schoolTypes[option],
+      function_name: "top_school_area",
+    };
 
-    const districts = [
-        "Central Coast",
-        "Northern Sydney", 
-        "South Eastern Sydney", 
-        "South Western Sydney", 
-        "Sydney", 
-        "Western Sydney", 
-        "Nepean Blue Mountains"
-    ]
+    const response = await axios.post(
+      "https://q50eubtwpj.execute-api.us-east-1.amazonaws.com/top_school_area",
+      requestBody,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    setLoading(false);
+    setSchools(JSON.parse(response.data.top_school_area));
+  };
 
-    const schoolTypes = ["Secondary School", "Primary School", "Infants School"];
+  const districts = [
+    "Central Coast",
+    "Northern Sydney",
+    "South Eastern Sydney",
+    "South Western Sydney",
+    "Sydney",
+    "Western Sydney",
+    "Nepean Blue Mountains",
+  ];
 
-    return (
-        <div className="page">
-            <h1>Properties near schools
-            </h1>
-            <p>Id:</p>
-            <input type="text" name="id" placeholder="Id" onChange={e => {
-                if (e.target.value !== "") {
-                    setId(e.target.value)
-                } else {
-                    setId(null)
-                }
-            }}/>
-            <p>School level:</p>
-            <select
-                id="type-dropdown"
-                value={option}
-                onChange={(e) => {
-                setOption(parseInt(e.target.value));
-                }}
-            >
-                <option value="0">Secondary School</option>
-                <option value="1">Primary School</option>
-                <option value="2">Infant School</option>
-            </select>
-            <p>District:</p>
-            <select
-                id="district-dropdown"
-                value={option1}
-                onChange={(e) => {
-                    setOption1(parseInt(e.target.value));
-                }}
-            >
-                {districts.map((district, index) => (
-                    <option key={index} value={index}>
-                        {district}
-                    </option>
-                ))}
-            </select>
-            <p>Radius(km):</p>
-            <input type="text" name="radius" placeholder="Radius" onChange={e => {
-                if (e.target.value !== "") {
-                    setRadius(e.target.value)
-                } 
-            }}/>
-            <button onClick={fetchData}>Fetch</button>
+  const schoolTypes = ["Secondary School", "Primary School", "Infants School"];
 
-            {
-                loading && <p>Loading...</p>
-            }
+  return (
+    <div className="page">
+      <h1>Properties near schools</h1>
+      <p>Id:</p>
+      <input
+        type="text"
+        name="id"
+        placeholder="Id"
+        onChange={(e) => {
+          if (e.target.value !== "") {
+            setId(e.target.value);
+          } else {
+            setId(null);
+          }
+        }}
+      />
+      <p>School level:</p>
+      <select
+        id="type-dropdown"
+        value={option}
+        onChange={(e) => {
+          setOption(parseInt(e.target.value));
+        }}
+      >
+        <option value="0">Secondary School</option>
+        <option value="1">Primary School</option>
+        <option value="2">Infant School</option>
+      </select>
+      <p>District:</p>
+      <select
+        id="district-dropdown"
+        value={option1}
+        onChange={(e) => {
+          setOption1(parseInt(e.target.value));
+        }}
+      >
+        {districts.map((district, index) => (
+          <option key={index} value={index}>
+            {district}
+          </option>
+        ))}
+      </select>
+      <p>Radius(km):</p>
+      <input
+        type="text"
+        name="radius"
+        placeholder="Radius"
+        onChange={(e) => {
+          if (e.target.value !== "") {
+            setRadius(e.target.value);
+          }
+        }}
+      />
+      <button onClick={fetchData}>Fetch</button>
 
-            {
-                schools && 
-                <table>
-                    <thead>
-                    <tr>
-                        <th>School</th>
-                        <th>Number of Properties</th>
-                        <th>Average Property Price</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {schools.map((school, index) => (
-                        <tr key={index}>
-                        <td>{school.school}</td>
-                        <td>{school.num_properties}</td>
-                        <td>
-                            {school.avg_property_price.toLocaleString("en-US", {
-                            style: "currency",
-                            currency: "USD",
-                            })}
-                        </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                 </table>
-            }
-        </div>
-    )
-}
+      {loading && <p>Loading...</p>}
 
-export default TopSchoolArea
+      {schools && (
+        <table>
+          <thead>
+            <tr>
+              <th>School</th>
+              <th>Number of Properties</th>
+              <th>Average Property Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {schools.map((school, index) => (
+              <tr key={index}>
+                <td>{school.school}</td>
+                <td>{school.num_properties}</td>
+                <td>
+                  {school.avg_property_price.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default TopSchoolArea;
