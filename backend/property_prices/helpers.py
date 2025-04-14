0f9e531b4bf9451403f28e9c9565a3b38b335cb1
Joel Lum <joellumzh@gmail.com>
2025-04-14 14:09:04 +1000

    Modified average property prices function to handle any data inputs, and adjusted accompanying unit test

--- backend/property_prices/helpers.py
+++ backend/property_prices/helpers.py
@@ -1,41 +1,31 @@
+# Sample filters input
+# 1. Filters that are strings will be filtered for that specific type
+# 2. Filters that are dictionaries (of min/max) will be filtered for that specific range
+
+# filters = {
+#     "suburb": string,
+#     "date_sold": {
+#       "min": date_object,
+#       "max": date_object,
+#     },
+#     "price": {
+#       "min": int,
+#       "max": int,
+#     }
+# }
 def avg_property_price(df, filters=None):
     if filters is not None:
-        # Filter by price range
-        if (price_range := filters.get("price_range", None)) is not None:
-            if price_range["min_price"] is not None:
-                df = df[df["price"] >= price_range["min_price"]]
-            if price_range["max_price"] is not None:
-                df = df[df["price"] <= price_range["max_price"]]
+        for key, condition in filters.items():
+            if key not in df.columns: continue
 
-        # Filter by date of property sold
-        if (date_range := filters.get("date_range", None)) is not None:
-            if date_range["date_sold_after"] is not None:
-                df = df[df["date_sold"] >= date_range["date_sold_after"]]
-            if date_range["date_sold_before"] is not None:
-                df = df[df["date_sold"] <= date_range["date_sold_before"]]
-
-        # Filter by suburb
-        if (suburb := filters.get("suburb", None)) is not None:
-            df = df[df["suburb"] == suburb]
-
-        # Filter by property features
-        if (property_features := filters.get("property_features", None)) is not None:
-            if property_features["num_bath"] is not None:
-                df = df[df["num_bath"] >= property_features["num_bath"]]
-            if property_features["num_bed"] is not None:
-                df = df[df["num_bed"] >= property_features["num_bed"]]
-            if property_features["num_parking"] is not None:
-                df = df[df["num_parking"] >= property_features["num_parking"]]
-
-        # Filter by property size
-        if (property_size := filters.get("property_size", None)) is not None:
-            if property_size["min_size"] is not None:
-                df = df[df["property_size"] >= property_size["min_size"]]
-            if property_size["max_size"] is not None:
-                df = df[df["property_size"] >= property_size["max_size"]]
-
-        # Filter by property type
-        if (property_type := filters.get("property_type", None)) is not None:
-            df = df[df["type"].str.lower() == property_type.lower()]
+            if isinstance(condition, dict):
+                # Apply range filter using "min" and/or "max"
+                if "min" in condition and condition["min"] is not None:
+                    df = df[df[key] >= condition["min"]]
+                if "max" in condition and condition["max"] is not None:
+                    df = df[df[key] <= condition["max"]]
+            else:
+                # Apply exact match filter
+                df = df[df[key] == condition]
 
     return float(df["price"].mean()) if not df.empty else 0
