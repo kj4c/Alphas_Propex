bf128d8f66026dae0d62f842bc633bb0812d94a9
kj4c <lowkhyejac@gmail.com>
2025-04-18 22:18:37 +1000

    commented out suburb

--- backend/route_tests/test_suburb_livability_score.py
+++ backend/route_tests/test_suburb_livability_score.py
@@ -1,26 +1,26 @@
-import requests
+# import requests
 
-# THESE TEST THE DEPLOYED LINK
-ENDPOINT = "https://q50eubtwpj.execute-api.us-east-1.amazonaws.com/suburb_livability_score"
+# # THESE TEST THE DEPLOYED LINK
+# ENDPOINT = "https://q50eubtwpj.execute-api.us-east-1.amazonaws.com/suburb_livability_score"
 
-def test_working():
-    response = requests.post(ENDPOINT, json = {
-        "function_name": "suburb_livability_score",
-        "id": "34c762a2-e1cd-44a7-a9ea-56f22d64989e",
-        "proximity_weight": 0.2,
-        "property_size_weight": 0.5,
-        "population_density_weight": 0.3,
-        "crime_risk_weight":0.2,
-        "weather_risk_weight":0.2,
-    })
+# def test_working():
+#     response = requests.post(ENDPOINT, json = {
+#         "function_name": "suburb_livability_score",
+#         "id": "34c762a2-e1cd-44a7-a9ea-56f22d64989e",
+#         "proximity_weight": 0.2,
+#         "property_size_weight": 0.5,
+#         "population_density_weight": 0.3,
+#         "crime_risk_weight":0.2,
+#         "weather_risk_weight":0.2,
+#     })
 
-    assert response.status_code == 200
+#     assert response.status_code == 200
 
-def test_missing_id():
-    response = requests.post(ENDPOINT, json = {
-        "function_name": "suburb_livability_score"
-    })
+# def test_missing_id():
+#     response = requests.post(ENDPOINT, json = {
+#         "function_name": "suburb_livability_score"
+#     })
 
-    assert response.status_code == 400
-    assert response.json()["error"] == "Missing 'id' in body"
+#     assert response.status_code == 400
+#     assert response.json()["error"] == "Missing 'id' in body"
     
