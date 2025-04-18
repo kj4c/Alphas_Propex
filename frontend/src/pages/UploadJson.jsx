8cee7183a845a4c9038a8e7f4851d1df4a91ae37
kj4c <lowkhyejac@gmail.com>
2025-04-18 23:11:32 +1000

    fixed upload_json

--- frontend/src/pages/UploadJson.jsx
+++ frontend/src/pages/UploadJson.jsx
@@ -24,15 +24,29 @@ const UploadJson = () => {
     reader.onload = async (e) => {
       try {
         const jsonObject = JSON.parse(e.target.result);
+        const body = {
+          "function_name": "upload_json",
+        }
 
         const url = "https://q50eubtwpj.execute-api.us-east-1.amazonaws.com/upload_json";
 
-        const response = await axios.post(url, jsonObject, {
+        const response = await axios.post(url, body, {
           headers: {
             "Content-Type": "application/json",
           },
         });
         setRet(response.data)
+        console.log("Response:", response.data);
+
+        // make another axios request given the correct URL
+        const uploadUrl = response.data.upload_url;
+        console.log("Upload URL:", uploadUrl);
+        axios.put(uploadUrl, jsonObject, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
         setLoading(false)
       } catch (error) {
         console.error("Error uploading file:", error);
@@ -57,11 +71,10 @@ const UploadJson = () => {
         }
         {
             ret != null && <>
-                <h3>{ret.message}</h3>
-                <h4>Upload URL</h4>
-                <p>{ret.upload_url}</p>
-                <h4>Data ID</h4>
+                <h3>Successfully uploaded json to the S3 Bucket</h3>
+                <h4>Here is your data ID</h4>
                 <p>{ret.data_id}</p>
+                <p>Please remember this as we will not show this again!</p>
             </>
         }
     </div>
